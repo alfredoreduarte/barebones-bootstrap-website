4b6099064e27f7993c9fc22a8c3909209d9f0567
Alfredo Re <alfredoreduarte@gmail.com>
2015-06-21 22:04:56 -0400

    Initial commit

--- _head.php
+++ _head.php
@@ -4,10 +4,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-	<title>WebiGallery</title>
+	<title>Title</title>
 	<link rel="icon" href="images/favicon.png">
 	<!-- Bootstrap -->
-	<link href="application.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
