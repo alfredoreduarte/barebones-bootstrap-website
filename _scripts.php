d187e40a4dd6b0cf2afae83c4093852a8d0ecbb1
Alfredo Re <alfredoreduarte@gmail.com>
2015-08-27 16:54:01 -0400

    bumped jquery to 2.1.3

--- _scripts.php
+++ _scripts.php
@@ -1,4 +1,4 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script type="text/javascript">
 function addScript(src) {
